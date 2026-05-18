874168eb37192ff2b54a25dcc5bebace117e4778
Angelina Ankovic <angelinaankovic@gmail.com>
2026-05-18 19:33:10 -0300

    Desarrollo y calculos de funcion cuadratica

--- consigna2Angy.py
+++ consigna2Angy.py
@@ -2,9 +2,6 @@ import math
 
 funcion= int(input("¡Bienvenido al sistema de informacion de funciones lineales!\n Elija a continuacion sobre que tipo de funcion desea trabajar:\n1- Funcion lineal\n2- Funcion cuadratica\nOpcion seleccionada: "))
 
-#def menu2():
-
-
 if funcion == 1:
     m= float(input('----------------------------------------\nDada la ecuacion de funcion lineal "y= mx + b" a continuacion, ingrese los valores correspondientes a cada dato solicitado\nIngrese el valor de m: '))
     bLineal= float(input('Ingrese el valor de "b": '))
@@ -43,3 +40,44 @@ if funcion == 1:
     print(f"- Dominio: {dominio}")
     print(f"- Rango: {rango}")
 
+elif funcion == 2:
+    print('----------------------------------------\nDada la ecuacion de funcion cuadratica "y= ax² + bx + c" a continuacion, ingrese los valores correspondientes a cada dato solicitado.')
+    a= float(input("Ingrese el valor de a: "))
+    bCuadratica= float(input('Ingrese el valor de "b": '))
+    c= float(input('Ingrese el valor de "c": '))
+
+    if a == 0:
+        print("El coeficiente 'a' no puede ser 0 en una funcion cuadratica.")
+    else:
+        vx= -bCuadratica / (2 * a)
+        vy= a * (vx)**2 + bCuadratica * vx + c
+
+        if a > 0:
+            concavidad= "Concava hacia arriba"
+            rango= f"{vy:.2f}, +infinito" #Va del vertice hacia arriba
+        elif a < 0:
+            concavidad= "Concava hacia abajo"
+            rango= f"-infinito, {vy:.2f}" #Va del -infinito hasta el vertice
+
+        discriminante= bCuadratica**2 - 4*a*c
+
+        if discriminante > 0:
+            raizD= discriminante**0.5 #Elevar a 0.5 es lo mismo que hacer raiz cuadrada
+            raiz1= (-bCuadratica + raizD) / (2*a)
+            raiz2= (-bCuadratica - raizD) / (2*a)
+            raizCuadratica= f"La funcion tiene 2 raices: x1= {raiz1:.2f} y x2= {raiz2:.2f}"
+        elif discriminante < 0:
+            raizCuadratica= "La funcion no corta al eje x (sus raices son imaginarias.)"
+        else:
+            raizCuadratica= f"La funcion solo tiene una sola raiz que coincide con el vertice: {vx}"
+
+        print(f"----------------------------------------\nLa ecuacion ingresada es: y= {a}x² + {bCuadratica}x + c")
+        print("Los datos reelevantes de su función son:")
+        print(f"- Ordenada al origen: {c}")
+        print(f"- Vertice: ({vx:.2f}, {vy:.2f})")
+        print(f"- Concavidad: {concavidad}")
+        print(f"- Raices: {raizCuadratica}")
+        print(f"- Dominio: -infinito, +infinito") #Para todas las funciones cuadraticas, el dominio es el mismo
+        print(f"- Rango: {rango}")
+else:
+    print("Ingrese solo un numero correspondiente a una opcion.")
